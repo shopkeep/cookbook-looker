66b777e267697f92920e0725471808115366eee2
James Awesome <awesome@shopkeep.com>
2015-02-03 11:31:02 -0500

    Rewrote files resource spec for s3

--- spec/recipes/_files_spec.rb
+++ spec/recipes/_files_spec.rb
@@ -2,10 +2,16 @@ require_relative '../spec_helper'
 
 
 describe 'looker::_files' do
+  s3_url = 'https://s3.amazonaws.com/download.looker.com'
+  s3_startup_file = 'aeHee2HiNeekoh3uIu6hec3W/looker'
+  s3_jar_file = 'aeHee2HiNeekoh3uIu6hec3W/looker-latest.jar'
+
   let(:chef_run) do 
     ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '14.04') do |node|
       node.set['looker']['home'] = LOOKER_HOME
-      node.set['looker']['file_cookbook'] = 'looker_wrapper'
+      node.set['looker']['s3']['url'] = s3_url
+      node.set['looker']['s3']['startup_file'] = s3_startup_file
+      node.set['looker']['s3']['jar_file'] = s3_jar_file
     end.converge(described_recipe) 
   end
 
@@ -16,30 +22,39 @@ describe 'looker::_files' do
     )
   end
 
-  it 'Places the looker startup script' do
-    expect(chef_run).to create_cookbook_file_if_missing("#{LOOKER_HOME}/looker/looker").with(
-      'source' => 'looker',
-      'owner' => 'looker',
-      'group' => 'looker',
-      'mode' => 0750
+  it 'Gets startup script from s3' do
+    expect(chef_run).to create_s3_file("#{LOOKER_HOME}/looker/looker").with(
+      s3_url: s3_url,
+      remote_path: s3_startup_file,
+      owner: 'looker',
+      group: 'looker',
+      mode: 0750
     )
   end
 
-  it 'Creates a symlink at /etc/init.d/looker to the looker start script' do
-    expect(chef_run).to create_link('/etc/init.d/looker').with(
-      'to' => "#{LOOKER_HOME}/looker/looker",
-      'owner' => 'looker',
-      'group' => 'looker'
+  context 'if startup script is already downloaded' do
+    before { allow(File).to receive(:exist?).with("#{LOOKER_HOME}/looker/looker") }
+    it 'does not get startup script from s3' do
+      expect(chef_run).to_not create_s3_file("#{LOOKER_HOME}/looker/looker")
+    end
+  end
+
+  it 'Gets looker.jar from s3' do
+    expect(chef_run).to create_s3_file("#{LOOKER_HOME}/looker/looker").with(
+      s3_url: s3_url,
+      remote_path: s3_startup_file,
+      owner: 'looker',
+      group: 'looker',
+      mode: 0750
     )
   end
 
-  it 'Places looker.jar' do
-    expect(chef_run).to create_cookbook_file_if_missing("#{LOOKER_HOME}/looker/looker.jar").with(
-      'source' => 'looker.jar',
-      'owner' => 'looker',
-      'group' => 'looker',
-      'mode' => 0750
-    )
+  context 'if looker.jar already exists' do
+    before { allow(File).to receive(:exist?).with("#{LOOKER_HOME}/looker/looker.jar") }
+    it 'does not get looket.jar from s3' do
+      expect(chef_run).to_not create_s3_file("#{LOOKER_HOME}/looker/looker.jar")
+    end
   end
+
 
 end
