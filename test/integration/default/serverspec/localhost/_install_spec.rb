72f580b9e90f5564c53a5f98f20402cbaf05b827
James Awesome <awesome@shopkeep.com>
2015-02-03 21:18:52 -0500

    Move user serverspec tests to _install_spec.rb

--- test/integration/default/serverspec/localhost/_install_spec.rb
+++ test/integration/default/serverspec/localhost/_install_spec.rb
@@ -4,6 +4,21 @@ describe 'looker:_install' do
 
   looker_run_dir = '/home/looker/looker'
 
+  describe group('looker') do
+    it { should exist }
+  end
+
+  describe user('looker') do
+    it { should exist }
+    it { should belong_to_group 'looker' }
+  end
+
+  describe file('/home/looker') do
+    it { should be_directory }
+    it { should be_owned_by 'looker' }
+    it { should be_grouped_into 'looker' }
+  end
+
   describe file(looker_run_dir) do
     it { should be_directory }
     it { should be_owned_by 'looker' }
