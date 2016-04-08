c1ad295c2e847c96fafe1d29c28bebdf0bf81ba8
James Awesome <awesome@shopkeep.com>
2016-04-08 15:36:28 -0400

    Specifically defined Service provider

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'awesome@shopkeep.com'
 license          'All rights reserved'
 description      'Installs/Configures looker'
 long_description 'Installs/Configures looker'
-version          '0.2.3'
+version          '0.2.4'
 
 depends 'apt'
 depends 'java'
